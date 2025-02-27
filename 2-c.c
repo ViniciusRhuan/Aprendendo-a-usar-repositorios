4ac28602bb4a0c87db46b56981786ef9a9cd7f72
Vinicius Silva <viniciusrhuan9@gmail.com>
2025-02-27 09:37:18 -0300

    Adicionando 2c e modificando 2b

--- 2-c.c
+++ 2-c.c
@@ -17,9 +17,9 @@ int main() {
         "orw %%dx, %%ax;"      // Combina 'b' nos 16 bits inferiores de EAX
         "movl %%eax, %0;"      // Armazena o resultado final em 'c'
 
-        "=m" (c)             // Saída (variável 'c')
-        "=m" (a), "m" (b)     // Entradas (variáveis 'a' e 'b')
-        "%ax", "%dx"         // Registradores modificados
+        : "=m" (c)             // Saída (variável 'c')
+        : "m" (a), "m" (b)     // Entradas (variáveis 'a' e 'b')
+        : "%ax", "%dx"         // Registradores modificados
     );
 
     printf("Resultado final: %d (0x%X)\n", c, c);
